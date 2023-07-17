cb6aee65eb54bda37c0bcb1c1f5faf3962482465
Gleb Remniov <g.remniov@gmail.com>
2023-07-17 19:33:43 +0300

    Initial commit

---
+++ src/main/java/com/thekitchen/incomecalculator/IncomeCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.thekitchen.incomecalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncomeCalculatorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(IncomeCalculatorApplication.class, args);
+  }
+
+}
