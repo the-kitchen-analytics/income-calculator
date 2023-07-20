1ad77124d45489d7875f81eac57bbfec90781c97
Gleb Remniov <g.remniov@gmail.com>
2023-07-20 23:03:13 +0300

    Minor refactoring (#7)

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,6 +26,7 @@ repositories {
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-web")
+  implementation("org.springframework.boot:spring-boot-starter-actuator")
   developmentOnly("org.springframework.boot:spring-boot-devtools")
   compileOnly("org.projectlombok:lombok")
   annotationProcessor("org.projectlombok:lombok")
