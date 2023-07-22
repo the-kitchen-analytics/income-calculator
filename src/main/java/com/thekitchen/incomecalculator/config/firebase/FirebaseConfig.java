0f01110bfb7908a5a39b7caac4143714044d063a
Gleb Remniov <g.remniov@gmail.com>
2023-07-22 23:23:58 +0300

    Add collection name enum

--- src/main/java/com/thekitchen/incomecalculator/config/firebase/FirebaseConfig.java
+++ src/main/java/com/thekitchen/incomecalculator/config/firebase/FirebaseConfig.java
@@ -3,12 +3,24 @@ package com.thekitchen.incomecalculator.config.firebase;
 import com.google.cloud.firestore.Firestore;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.cloud.FirestoreClient;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 
 @Configuration
 public class FirebaseConfig {
+
+  @Getter
+  @RequiredArgsConstructor
+  public enum CollectionName {
+    RECEIPTS("receipts"),
+    PROCEDURES("procedures_v2"),
+    USERS("userDetails");
+
+    private final String value;
+  }
 
   @Bean
   FirebaseApp firebaseApp(FirebaseProperties properties, FirebaseAppFactory factory) {
