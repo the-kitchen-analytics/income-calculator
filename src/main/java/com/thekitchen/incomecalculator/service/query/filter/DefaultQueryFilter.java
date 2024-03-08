5cf8105884fed0bf5f24c8cbd334bd3e631b33f5
Hleb Ramniou <g.remniov@gmail.com>
2024-03-08 23:00:37 +0200

    Remove unused files (#13)

--- src/main/java/com/thekitchen/incomecalculator/service/query/filter/DefaultQueryFilter.java
+++
@@ -1,9 +0,0 @@
-package com.thekitchen.incomecalculator.service.query.filter;
-
-public interface DefaultQueryFilter<P, V> extends QueryFilter<P, V> {
-
-  @Override
-  default boolean isApplicable(P params) {
-    return false;
-  }
-}
